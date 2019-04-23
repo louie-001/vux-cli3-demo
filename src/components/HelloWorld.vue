89a0663f61258408205a60521497ce6e2824c25e
louie-001 <louie@xonro.com>
2019-04-23 10:19:19 +0800

    init project

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,41 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <group>
+    <cell title="cell title" is-link/>
+
+    <tabbar style="position: fixed">
+      <tabbar-item>
+        <span slot="label">wehat</span>
+        <emotion slot="icon" is-gif>微笑</emotion>
+      </tabbar-item>
+      <tabbar-item>
+        <span slot="label">message</span>
+        <emotion slot="icon" is-gif>得意</emotion>
+      </tabbar-item>
+      <tabbar-item>
+        <span slot="label">news</span>
+        <emotion slot="icon" is-gif>鼓掌</emotion>
+      </tabbar-item>
+    </tabbar>
+  </group>
 </template>
 
 <script>
+import {
+    Group,
+    Cell,
+    Tabbar,
+    TabbarItem,
+    WechatEmotion
+} from 'vux'
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  components: {
+      Group,
+      Cell,
+      Tabbar,
+      TabbarItem,
+      Emotion: WechatEmotion
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    configureWebpack: (config) => {
+        require('@vux/loader').merge(config, {
+            plugins: [
+                'vux-ui'
+            ]
+        })
+    }
+}
